40d769453f57024e826ea06e82ffe20447ab0559
Tim Gilbert <timgilbert@gmail.com>
2019-03-21 13:50:14 -0400

    Code looks ok

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-classpath-jar "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A leiningen plugin to work around Windows classpath limits"
+  :url "https://github.com/timgilbert/lein-classpath-jar"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
--- src/lein_classpath_jar/plugin.clj
+++ src/lein_classpath_jar/plugin.clj
@@ -4,40 +4,53 @@
             [clojure.string :refer [blank?]]
             [leiningen.core.main :refer [debug]]
             [clojure.java.io :as jio]
-            [clojure.string :as str])
+            [clojure.string :as string])
   (:import (java.security MessageDigest)
-           (java.nio.file Files Paths Path LinkOption)
+           (java.nio.file Files Paths Path)
            (java.util.jar JarOutputStream Attributes$Name Manifest)
            (java.io File)))
 
 ;; cf https://github.com/nickgieschen/lein-extend-cp/blob/master/src/lein_extend_cp/plugin.clj
 
-(defn md5 [string]
-  (doto (MessageDigest/getInstance "MD5")
-    (.digest (.getBytes string))))
+;; Cribbing from https://github.com/tebeka/clj-digest/blob/master/src/digest.clj here
+(defn md5
+  "Produce an MD5 checksum similar to `B80AC5C8E229CFFD374E8A74168225CF`"
+  [^String string]
+  (let [digest (doto (MessageDigest/getInstance "MD5")
+                 (.update (.getBytes string "UTF-8")))
+        value  (BigInteger. 1 (.digest digest))]
+    (format (str "%0" (* 2 (.getDigestLength digest)) "X") value)))
+
 
 (defn classpath-jar
   [classpath]
   (str (md5 classpath) ".jar"))
 
 (defn ^Path jar-path-for-classpath [classpath]
-  ;; TODO: implement me
-  (Paths/get (into-array String [(System/getProperty "user.home")
-                                 ".cpcache"
+  (Paths/get (System/getProperty "user.dir")
+             (into-array String [".cpcache"
                                  (classpath-jar classpath)])))
 
 (defn- manifest-classpath
   "Translate the classpath into a format suitable for a jar manifest, by changing all
   of the paths to file:// URIs and separating the list by single spaces."
   [classpath]
-  (->> (for [path (str/split classpath (re-pattern File/pathSeparator))]
+  (->> (for [path (string/split classpath (re-pattern File/pathSeparator))]
         (-> path jio/file .getAbsoluteFile .toURI .toString))
-       (str/join " ")))
+       (string/join " ")))
+
+(defn create-cpcache-dir!
+  "Create the .cpcache directory (in the current directory, assumed to be the project root) if
+  it doesn't exist."
+  []
+  (Files/createDirectories (Paths/get (System/getProperty "user.dir") (into-array [".cpcache"]))
+                           (into-array java.nio.file.attribute.FileAttribute [])))
 
 (defn create-classpath-jar!
   "Create a \"pathing jar\" - a jar file containing only a classpath in its manifest,
   useful in systems where command-line arguments have a limited length."
   [^Path jar-path classpath]
+  (create-cpcache-dir!)
   (let [manifest (Manifest.)]
    (doto (.getMainAttributes manifest)
     (.put Attributes$Name/CLASS_PATH (manifest-classpath classpath))
